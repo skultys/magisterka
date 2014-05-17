619891a8d011bff58b62b7f0c9471a6dea641a08
skultys <saffron.sk@gmail.com>
2014-05-18 01:12:13 +0200

    stworzenie projektu

---
+++ magisterka/magisterka/IEvolutionAlgorithm.cs
@@ -0,0 +1,109 @@
+﻿
+/////////////////////////////////////////////////////////////////////////////
+// C# Version Copyright (c) 2011   Wojciech Chmiel                         //
+// All rights reserved                                                     //
+/////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace OptimisationClassLibrary
+{
+    public interface IEvolutionAlgorithm : IOptimisationAlgorithm
+    {
+       IPopulation Population{get;set;}
+    }
+
+    public interface IPopulation : IEnumerable
+    {
+        int Size { get; }
+        //static IPopulation operator +(IPopulation pop, ISolution sol);
+        int Add(ISolution sol);
+        ISolution this[int index] { get; set; }
+
+        /// <summary>
+        /// Number of solution in population
+        /// </summary>
+        int CurrentSize { get; }
+
+        /// <summary>
+        /// Remove solution on index position
+        /// </summary>
+        /// <param name="idx"></param>
+        void Remove(int idx);
+
+
+    }
+
+
+    public interface IOptimisationAlgorithm
+    {
+        void InitRandomPopulation();
+        /// <summary>
+        /// Run algorithm
+        /// </summary>
+        void Execute();
+        /// <summary>
+        /// Check stop
+        /// </summary>
+        /// <returns></returns>
+        bool Stop();
+        /// <summary>
+        /// Return best solution 
+        /// </summary>
+        /// <returns></returns>
+        ISolution GetBestSolution();
+
+        /// <summary>
+        /// Get solution criteria value
+        /// </summary>
+        /// <param name="solution"></param>
+        /// <returns></returns>
+        double EvaluateSolution(ISolution solution);
+    }
+
+
+
+    public interface IOptimisationAlgorithmPhyton
+    {
+    
+    
+    
+    
+    
+    }
+    
+
+
+
+
+
+
+    public interface ISolution
+    {
+        double Goal { get; set; }
+
+    }
+
+
+    interface IEvolutionaryOperator
+    {
+    }
+
+    interface IMutationOperator : IEvolutionaryOperator
+    {
+
+        ISolution Execute(IPopulation population);
+    }
+
+    interface ICrossoverOperator : IEvolutionaryOperator
+    {
+        ISolution[] Execute(IPopulation population);
+    }
+
+
+
+  
+}
---
+++ magisterka/magisterka/QGA.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OptimisationClassLibrary;
+using System.Collections;
+
+namespace magisterka
+{
+    class Solution : ISolution
+    {
+        public double Goal { set; get; }
+    }
+
+    class Population : IPopulation {
+        public int Size
+        {
+            get
+            {
+                return 0;
+            }
+        }
+        public int Add(ISolution sol)
+        {
+            return 0;
+        }
+        public ISolution this[int idex]
+        {
+            get
+            {
+                return new Solution();
+            }
+            set
+            {
+            }
+        }
+        public int CurrentSize
+        {
+            get
+            {
+                return 0;
+            }
+        }
+        public void Remove(int idx)
+        {
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (object o in this)
+            {
+                if (o == null)
+                {
+                    break;
+                }
+                yield return o;
+            }
+        }
+    }
+
+    class MutationOperator : IMutationOperator
+    {
+        public ISolution Execute(IPopulation population)
+        {
+            return new Solution();
+        }
+    }
+
+    class CrossoverOperator : ICrossoverOperator
+    {
+        public ISolution[] Execute(IPopulation population)
+        {
+            return new Solution[0];
+        }
+    }
+
+    class QGA : IEvolutionAlgorithm
+    {
+        public IPopulation Population { get; set; }
+        public double EvaluateSolution(ISolution solution)
+        {
+            return 0.0;
+        }
+        public void InitRandomPopulation() {
+        }
+        public void Execute()
+        {
+        }
+        public bool Stop()
+        {
+            return false;
+        }
+        public ISolution GetBestSolution()
+        {
+            return new Solution();
+        }
+
+    }
+}
